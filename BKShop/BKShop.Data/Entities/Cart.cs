76e35aedffb46dee391bd9c048ab4083dc36843a
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-06-21 00:39:48 +0700

    Add class model
    Brand, categoy, user, role, product,...

--- BKShop/BKShop.Data/Entities/Cart.cs
+++ BKShop/BKShop.Data/Entities/Cart.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BKShop.Data.Entities
 {
-    internal class Cart
+    public class Cart
     {
+        [Key]
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+        [Key]
+        public int ProductID { get; set; }
+        public Product Product { get; set; }
+        public int Amount { get; set; }
     }
 }
