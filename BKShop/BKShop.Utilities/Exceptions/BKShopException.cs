41cea2807ef495f979555c846721098492c8fddf
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-06-24 00:31:37 +0700

    Add: add project BKShop.Ultilities

---
+++ BKShop/BKShop.Utilities/Exceptions/BKShopException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKShop.Utilities.Exceptions
+{
+    public class BKShopException : Exception
+    {
+        public BKShopException()
+        {
+        }
+
+        public BKShopException(string message)
+            : base(message)
+        {
+        }
+
+        public BKShopException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
