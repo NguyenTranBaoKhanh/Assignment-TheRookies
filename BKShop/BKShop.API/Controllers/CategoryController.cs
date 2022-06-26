da5b7462335fc67747a53d39e64ea55ca5c1c3f1
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-06-27 01:45:21 +0700

    Update: CategoryController

--- BKShop/BKShop.API/Controllers/CategoryController.cs
+++ BKShop/BKShop.API/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
-﻿using BKShop.Data.EF;
+﻿using BKShop.Application.Interfaces;
+using BKShop.Data.EF;
 using BKShop.Data.Entities;
+using BKShop.ViewModels.Requests.Category;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BKShop.API.Controllers
@@ -9,31 +12,103 @@ namespace BKShop.API.Controllers
     public class CategoryController : ControllerBase
     {
         private readonly BKShopDbContext _context;
-        public CategoryController(BKShopDbContext context)
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService )
         {
-            _context = context;
+            _categoryService = categoryService;
         }
 
         [HttpGet]
-        public IActionResult Get()
+        [AllowAnonymous]
+        public async Task<IActionResult> Get()
         {
-            var categories = _context.Categories.ToList();
+            //var categories = _context.Categories.ToList();
+            var categories = await _categoryService.GetAllAsync();
             return Ok(categories);
         }
 
-        [HttpGet("{id}")]
-        public IActionResult GetById([FromRoute] int id)
+        [HttpGet("{Id}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetById(int Id) //[FromRoute] int id
         {
-            var categories = _context.Categories.FirstOrDefault(p => p.Id == id);
-            return Ok(categories);
+            try
+            {
+                //var categories = _context.Categories.FirstOrDefault(p => p.Id == id);
+                var category = await _categoryService.GetByIdAsync(Id);
+                if(category == null)
+                {
+                    return NotFound($"Cannot find a cake with Id: {Id}");
+                }
+                return Ok(category);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] Category categories)
+        public async Task<IActionResult> Create([FromBody] CategoryCreateRequest request)
         {
-            _context.Categories.Add(categories);
-            await _context.SaveChangesAsync();
-            return Ok(categories);
+            //_context.Categories.Add(categories);
+            //await _context.SaveChangesAsync();
+            var categoryId = await _categoryService.CreateAsync(request);
+            if(categoryId == 0)
+            {
+                return BadRequest();
+            }
+            var data = await _categoryService.GetByIdAsync(categoryId);
+            if(data == null)
+            {
+                return NotFound($"Cannot find a cake with Id: {categoryId}");
+            }
+            return Ok(data);
+        }
+
+        [HttpDelete("{Id}")]
+        [Authorize]
+        [AllowAnonymous]
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            try
+            {
+                var result = await _categoryService.DeleteAsync(Id);
+                if (result == 0)
+                {
+                    return BadRequest();
+                }
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] CategoryUpdateRequest request)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                var result = await _categoryService.UpdateAsync(request);
+
+                if (result == 0)
+                {
+                    return BadRequest();
+                }
+
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
     }
 }
