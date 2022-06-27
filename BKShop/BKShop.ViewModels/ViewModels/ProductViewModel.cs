5f8c3b958d287fe48fd307b1101eb631787828ea
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-06-27 10:12:53 +0700

    Add: ProductViewModel

---
+++ BKShop/BKShop.ViewModels/ViewModels/ProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKShop.ViewModels.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Capacuty { get; set; }
+        public string Stock { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public int CategoryId { get; set; }
+        public int BrandId { get; set; }
+    }
+}
