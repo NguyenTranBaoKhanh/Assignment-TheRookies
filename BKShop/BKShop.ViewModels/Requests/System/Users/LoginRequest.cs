ae2385faf05630667ce2b185776fc46a9c34169d
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-06-29 22:58:58 +0700

    Add: add UserViewModel, UserUpdateRequest, RegisterRequest, LoginRequest

---
+++ BKShop/BKShop.ViewModels/Requests/System/Users/LoginRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKShop.ViewModels.Requests.System.Users
+{
+    public class LoginRequest
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [Display(Prompt = "******")]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
