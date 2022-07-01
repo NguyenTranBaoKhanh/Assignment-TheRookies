8a6a03dc2fa364c00ca88e3f8d3038a1e86e3127
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-07-02 01:16:52 +0700

    Add: create AccountController.cs AccessoryControler.cs

---
+++ BKShop/BKShop.CustomersSite/Controllers/AccessoryController.cs
@@ -0,0 +1,41 @@
+﻿using BKShop.ApiIntegration.Interfaces;
+using BKShop.CustomersSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Refit;
+
+namespace BKShop.CustomersSite.Controllers
+{
+    public class AccessoryController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ICategoryApi _categoryApi;
+        private IBrandApi _brandApi;
+        private IProductApi _productApi;
+        private HomeViewModel _homeViewModel;
+
+        public AccessoryController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _categoryApi = RestService.For<ICategoryApi>("https://localhost:7297");
+            _brandApi = RestService.For<IBrandApi>("https://localhost:7297");
+            _productApi = RestService.For<IProductApi>("https://localhost:7297");
+            _homeViewModel = new HomeViewModel();
+
+        }
+        public async Task<IActionResult> Index()
+        {
+            var catgories = _categoryApi.GetAllAsync().GetAwaiter().GetResult();
+            var accessories = _categoryApi.GetAccessoryAsync().GetAwaiter().GetResult();
+            var brands = _brandApi.GetAllAsync().GetAwaiter().GetResult();
+            _homeViewModel.Categories = catgories;
+            _homeViewModel.Acessories = accessories;
+            _homeViewModel.Brands = brands;
+
+            var products = _productApi.GetByAccessoryAsync().GetAwaiter().GetResult();
+            _homeViewModel.Products = products;
+
+
+            return View(_homeViewModel);
+        }
+    }
+}
