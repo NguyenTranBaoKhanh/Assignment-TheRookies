e53b29fe528d411827a7a53fed3f704ae70376c4
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-07-02 01:17:06 +0700

    Update HomeController.cs

--- BKShop/BKShop.CustomersSite/Controllers/HomeController.cs
+++ BKShop/BKShop.CustomersSite/Controllers/HomeController.cs
@@ -42,14 +42,29 @@ namespace BKShop.CustomersSite.Controllers
         public async Task<IActionResult> Detail(int Id, string Color)
         {
             var catgories = _categoryApi.GetAllAsync().GetAwaiter().GetResult();
-            var accessory = _categoryApi.GetAccessoryAsync().GetAwaiter().GetResult();
+            var accessories = _categoryApi.GetAccessoryAsync().GetAwaiter().GetResult();
             var brands = _brandApi.GetAllAsync().GetAwaiter().GetResult();
-            var product = _productApi.GetByIdAsync(Id).GetAwaiter().GetResult();
-
-            _homeViewModel.Brands = brands;
+            var productsTop5 = _productApi.GetTop5Async().GetAwaiter().GetResult();
             _homeViewModel.Categories = catgories;
-            _homeViewModel.Acessories = accessory;
+            _homeViewModel.Acessories = accessories;
+            _homeViewModel.Brands = brands;
+            _homeViewModel.ProductTop5 = productsTop5;
+
+            var product = _productApi.GetByIdAsync(Id).GetAwaiter().GetResult();
             _homeViewModel.Product = product;
+            var brand = _brandApi.GetByIdAsync(product.BrandId).GetAwaiter().GetResult();
+            _homeViewModel.Brand = brand;
+
+            _homeViewModel.color_url = Color;
+            _homeViewModel.id_url = Id;
+
+            var group = product.Group;
+            var productByColorByGroup = _productApi.GetByColorAndGroupAsync(Color, group).GetAwaiter().GetResult();
+            var color = _productApi.GetColorByGroupAsync(group).GetAwaiter().GetResult();
+
+            _homeViewModel.ProductByColorAndGroup = productByColorByGroup;
+            _homeViewModel.Colors = color;
+
 
 
             return View(_homeViewModel);
