9f1ab57c082b95fae407781f51ccb957c81e24aa
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-07-05 21:58:05 +0700

    Create ReviewViewModel.cs

---
+++ BKShop/BKShop.ViewModels/ViewModels/ReviewViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKShop.ViewModels.ViewModels
+{
+    public class ReviewViewModel
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public int ProductId { get; set; }
+        public string Comment { get; set; }
+        [Range(1, 5)]
+        public int Star { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateModified { get; set; }
+        public DateTime? DateDeleted { get; set; }
+    }
+}
