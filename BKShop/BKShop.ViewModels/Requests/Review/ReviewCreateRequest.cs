81c5ca87eb295fa62a06885bdff0d663c003dbe2
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-07-05 21:58:59 +0700

    Add: CreateRequest and UpadateRequestReview

---
+++ BKShop/BKShop.ViewModels/Requests/Review/ReviewCreateRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKShop.ViewModels.Requests.Review
+{
+    public class ReviewCreateRequest
+    {
+        public Guid UserId { get; set; }
+        public int ProductId { get; set; }
+        public string Comment { get; set; }
+        public int Star { get; set; }
+    }
+}
---
+++ BKShop/BKShop.ViewModels/Requests/Review/ReviewUpdateRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKShop.ViewModels.Requests.Review
+{
+    public class ReviewUpdateRequest
+    {
+        public int Id { get; set; }
+        public string Comment { get; set; }
+        public int Star { get; set; }
+    }
+}
