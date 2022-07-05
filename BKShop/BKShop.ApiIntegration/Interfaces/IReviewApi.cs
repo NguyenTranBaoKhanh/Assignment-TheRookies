2941ed31e509a82056038420647b66e805e8e5f1
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-07-05 22:05:55 +0700

    Create IReviewApi.cs

---
+++ BKShop/BKShop.ApiIntegration/Interfaces/IReviewApi.cs
@@ -0,0 +1,23 @@
+﻿using BKShop.ViewModels.Requests.Review;
+using BKShop.ViewModels.ViewModels;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKShop.ApiIntegration.Interfaces
+{
+    public interface IReviewApi
+    {
+        [Get("/api/Review/product/{Id}")]
+        Task<List<ReviewViewModel>> GetByProductIdAsync(int Id);
+
+        [Post("/api/Review")]
+        Task<ReviewViewModel> CreateAsync(ReviewCreateRequest request);
+
+        [Get("/api/Review/AverageStar/{productId}")]
+        Task<double> GetAverageStarAsync(int productId);
+    }
+}
