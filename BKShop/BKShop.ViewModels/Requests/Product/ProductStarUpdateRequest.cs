3d1bce3477cd4c53c82f37618f7ba391da076fb3
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-07-06 21:23:26 +0700

    Create ProductStarUpdateRequest.cs

---
+++ BKShop/BKShop.ViewModels/Requests/Product/ProductStarUpdateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKShop.ViewModels.Requests.Product
+{
+    public class ProductStarUpdateRequest
+    {
+        public int productId { get; set; }
+        public double Stars { get; set; }
+    }
+}
