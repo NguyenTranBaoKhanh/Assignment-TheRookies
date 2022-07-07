53b9591344b2c04891f013cdef1f25ea0c973f06
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-07-07 11:22:31 +0700

    Create: create Component BestSeller

---
+++ BKShop/BKShop.CustomersSite/Pages/Shared/Components/BestSeller/BestSeller.cs
@@ -0,0 +1,27 @@
+﻿using BKShop.ApiIntegration.Interfaces;
+using BKShop.CustomersSite.Models;
+using BKShop.ViewModels.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Refit;
+
+namespace BKShop.CustomersSite.Pages.Shared.Components.BestSeller
+{
+    public class BestSeller : ViewComponent
+    {
+        private IProductApi _productApi;
+        public HomeViewModel _homeViewModel;
+
+        public BestSeller()
+        {
+            _productApi = RestService.For<IProductApi>("https://localhost:7297");
+            _homeViewModel = new HomeViewModel();
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var productsTop5 = _productApi.GetTop5Async().GetAwaiter().GetResult();
+            //_homeViewModel.ProductTop5 = productsTop5;
+
+            return View<List<ProductViewModel>>(productsTop5);
+        }
+    }
+}
