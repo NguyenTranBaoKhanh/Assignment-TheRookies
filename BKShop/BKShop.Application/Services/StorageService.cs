2ec524b35294a420880cdedb02eb98ebbb34fac9
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-07-18 00:03:03 +0700

    Add storageService

---
+++ BKShop/BKShop.Application/Services/StorageService.cs
@@ -0,0 +1,35 @@
+﻿using BKShop.Application.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKShop.Application.Services
+{
+    public class StorageService : IStorageService
+    {
+        private readonly string _userContentFolder;
+        private const string USER_CONTENT_FOLDER_NAME = "user-content";
+        public StorageService(IWebHostEnvironment webHostEnvironment)
+        {
+            _userContentFolder = Path.Combine(webHostEnvironment.WebRootPath, USER_CONTENT_FOLDER_NAME);
+        }
+
+        public Task DeleteFileAsync(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFileUrl(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveFileAsync(Stream mediaBinaryStream, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
