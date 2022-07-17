656c68993c01934eababe8341197ee7154b809bc
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-07-17 23:51:45 +0700

    Update ProductUpdateRequest.cs

--- BKShop/BKShop.ViewModels/Requests/Product/ProductUpdateRequest.cs
+++ BKShop/BKShop.ViewModels/Requests/Product/ProductUpdateRequest.cs
@@ -7,18 +7,18 @@ using System.Threading.Tasks;
 
 namespace BKShop.ViewModels.Requests.Product
 {
-    public class ProductUpdateRequest
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int CategoryId { get; set; }
-        public int BrandId { get; set; }
-        public string Color { get; set; }
-        public string Capacity { get; set; }
-        public int Price { get; set; }
-        public int Stock { get; set; }
-        public string Description { get; set; }
-        public string Group { get; set; }
-        public string Image { get; set; }
-    }
+  public class ProductUpdateRequest
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int CategoryId { get; set; }
+    public int BrandId { get; set; }
+    public string Color { get; set; }
+    public string Capacity { get; set; }
+    public int Price { get; set; }
+    public int Stock { get; set; }
+    public string Description { get; set; }
+    public string Group { get; set; }
+    public string? Image { get; set; }
+  }
 }
