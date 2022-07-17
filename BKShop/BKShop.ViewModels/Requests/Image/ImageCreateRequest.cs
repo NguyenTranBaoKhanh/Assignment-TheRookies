05d202c06ed446be0e8d6f450d3a01d6337eca27
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-07-17 23:51:15 +0700

    Create ImageCreateRequest.cs

---
+++ BKShop/BKShop.ViewModels/Requests/Image/ImageCreateRequest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKShop.ViewModels.Requests.Image
+{
+    public class ImageCreateRequest
+    {
+        public IFormFile files { get; set; }
+    }
+}
