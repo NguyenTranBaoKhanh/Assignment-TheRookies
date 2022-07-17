aed3bb7f2ba88b997c93960b39f5852417767156
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-07-17 23:59:52 +0700

    Update userService

---
+++ BKShop/BKShop.Application/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using BKShop.ViewModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKShop.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<List<UserViewModel>> GetAllAsync();
+        Task<UserViewModel> GetByIdAsync(Guid Id);
+    }
+}
---
+++ BKShop/BKShop.Application/Services/UserService.cs
@@ -0,0 +1,51 @@
+﻿using BKShop.Application.Interfaces;
+using BKShop.Data.EF;
+using BKShop.ViewModels.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKShop.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly BKShopDbContext _context;
+
+        public UserService(BKShopDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<UserViewModel>> GetAllAsync()
+        {
+            return await _context.Users.Select(user => new UserViewModel()
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Phone = user.Phone,
+                Address = user.Address,
+                Email = user.Email,
+
+            }).ToListAsync();
+        }
+
+        public async Task<UserViewModel> GetByIdAsync(Guid Id)
+        {
+            var user = await _context.Users.Where(user => user.Id == Id).FirstOrDefaultAsync();
+            if(user == null)
+            {
+                return null;
+            }
+            return new UserViewModel()
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Phone = user.Phone,
+                Address = user.Address,
+                Email = user.Email
+            };
+        }
+    }
+}
