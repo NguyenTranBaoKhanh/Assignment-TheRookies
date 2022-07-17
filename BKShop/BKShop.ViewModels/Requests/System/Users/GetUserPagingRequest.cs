a0ae0d1c269367d10b295967d34f63d2f1ab337e
NguyenTranBaoKhanh <76188911+NguyenTranBaoKhanh@users.noreply.github.com>
2022-07-17 23:51:57 +0700

    Create GetUserPagingRequest.cs

---
+++ BKShop/BKShop.ViewModels/Requests/System/Users/GetUserPagingRequest.cs
@@ -0,0 +1,14 @@
+﻿using BKShop.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKShop.ViewModels.Requests.System.Users
+{
+    public class GetUserPagingRequest : PagingRequestBase
+    {
+        public string Keyword { get; set; }
+    }
+}
